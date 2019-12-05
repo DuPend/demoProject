932d5a091504b2eb35485735e5d7ca88b6f790c8
dupeng <1710496709@qq.com>
2019-12-05 14:23:43 +0800

    TokenUtil删除没用的import

--- patentmanager-common/src/main/java/com/xinghuo/common/utils/TokenUtil.java
+++ patentmanager-common/src/main/java/com/xinghuo/common/utils/TokenUtil.java
@@ -7,9 +7,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.sun.istack.internal.Nullable;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
